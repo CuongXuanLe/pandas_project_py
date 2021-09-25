53d5db137b44e43932ba2adab0c2d9dac90595c0
Cuong-lee32 <cuong.le200222@vnuk.edu.vn>
2021-09-25 19:50:01 +0700

    _final project_

--- chess.py
+++ chess.py
@@ -1,22 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import dask.dataframe as dd
 
-# Choose your input data: (2pts)
-#    Ví dụ: trong challenge 1, có input data là 3 files csv
-#              trong challenge 2, có input data là một file hình ảnh bài làm trắc nghiệm
+# add -> commit -> push
 
-# Create 1 problem statement and 10 questions for the input data (ranking from easy to difficult): (3pts)
-#    Tạo ra 10  hỏi hợp lý giải quyết vấn đề và định nghĩa vấn đề liên quan tới dữ liệu,
-#    tham khảo cách đặt câu hỏi trong challenge 1 và challenge 2
-# 3. Solve your 10 questions (3pts)
-# 4. Coding style (1pts)
-# 5. Github (1pts)
+df = dd.read_csv('chess_games_large.csv')
 
-
-data = pd.read_csv('chess_games.csv', low_memory=False)
-data.head()
-
-#chunksize = 50000000
-#for chunk in pd.read_csv("chess_games.csv", chunksize=chunksize):
-#    process(chunk)
+# print(df.info())
+df_1 = df.groupby('Event').WhiteRatingDiff.mean().compute()
+print(df_1)
+df_2 = df.groupby('Event').BlackRatingDiff.mean().compute()
+print(df_2)
